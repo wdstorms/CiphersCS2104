6f9dd1f65521a356fe54857f6cf7fc8f8bbad1a6
henrykozurek <hkozurek@gmail.com>
2021-09-08 16:56:58 -0400

    added to cipher

--- henry-client.py
+++ henry-client.py
@@ -2,9 +2,9 @@ import Cipher
 c1 = Cipher.KeyCipher()
 c2 = Cipher.CaesarCipher()
 c3 = Cipher.Atbash()
-enc = 'ZGYZHSRHHRNKOVYFGVZHBGLYIVZP'
-key = 'Atbash'
-print(c3.decrypt(enc))
+c4 = Cipher.Base64()
+enc = 'a'
+c4.decrypt(enc)
 
 
 
